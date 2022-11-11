26e75a411477ec1ea106211ce0b6f089a5b6658f
zoey-yjj <zoeyyyjj@gmail.com>
2022-11-11 08:12:19 +0800

    add chat user, tests

--- 3_subscriber.cpp
+++ 3_subscriber.cpp
@@ -56,8 +56,38 @@ public:
     };
 };
 
+class ChatUser : public Subscriber {
+    std::string userName;
+public:
+    ChatUser(const std::string &userName) : userName(userName) {};
+    void notify(const std::string &publisherName, const std::string &message) override {
+        std::cout << userName << " received a new message from " << publisherName << ": " << message << "\n";
+    }
+    std::string getName() override { return userName; };
+};
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    ChatUser *user1 = new ChatUser("Jim");
+    ChatUser *user2 = new ChatUser("Barb");
+    ChatUser *user3 = new ChatUser("Hannah");
+
+    ChatGroup *group1 = new ChatGroup("Gardening group");
+    ChatGroup *group2 = new ChatGroup("Dog-lovers group");
+
+    group1->subscribe(user1);
+    group1->subscribe(user2);
+
+    group2->subscribe(user2);
+    group2->subscribe(user3);
+
+    group1->publish("Special sale on gardening supplies.");
+    group2->publish("It's national dog day everyone.");
+
+    delete user1;
+    delete user2;
+    delete user3;
+    delete group1;
+    delete group2;
+
     return 0;
 }
