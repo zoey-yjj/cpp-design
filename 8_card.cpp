4c883ff39d990a7fcc5b102a6d70c8443aaf9e9f
zoey-yjj <zoeyyyjj@gmail.com>
2022-11-13 14:06:06 +0800

    add birthday, new year card template, testing

--- 8_card.cpp
+++ 8_card.cpp
@@ -35,8 +35,31 @@ public:
     }
 };
 
+class BirthdayCardTemplate : public GreetingCardTemplate {
+protected:
+    std::string occasion() override {
+        return "Happy birthday!! Hope you have a wonderful day and lots of cake.\n";
+    }
+};
+
+class NewYearsCardTemplate : public GreetingCardTemplate {
+protected:
+    std::string intro(const std::string & to) override {
+        return to + "!!!\n";
+    }
+    std::string occasion() override {
+        return "Happy New Years!!!! See you at the gym tomorrow!\n";
+    }
+};
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    GreetingCardTemplate gct;
+    BirthdayCardTemplate bct;
+    NewYearsCardTemplate nyct;
+    
+    std::cout << "Here's a regular greeting card:\n\n" << gct.generate("Bob", "Jane") << "\n";
+    std::cout << "Here's a birthday card:\n\n" << bct.generate("Bob", "Jane") << "\n";
+    std::cout << "Here's a new year's card:\n\n" << nyct.generate("Bob", "Jane") << "\n";
+    
     return 0;
 }
