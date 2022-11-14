2d52a1f097090aa7dc15158439ff64f39691b886
zoey-yjj <zoeyyyjj@gmail.com>
2022-11-14 12:28:16 +0800

    number done

--- 11_number.cpp
+++ 11_number.cpp
@@ -54,8 +54,37 @@ public:
     }
 };
 
+class NumberCollection {
+    std::vector<int> numbers;
+public:
+    NumberCollection(const std::vector<int> & numbers) : numbers(numbers) {};
+    NumberIterator *getForwardsIterator() { return new ForwardsIterator(numbers); };
+    NumberIterator *getBackwardsIterator() { return new BackwardsIterator(numbers); };
+};
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::vector<int> numbers = { 1, 2, 3, 4, 5, 6, 7 };
+    NumberCollection nc(numbers);
+    
+    NumberIterator *fi = nc.getForwardsIterator();
+    NumberIterator *bi = nc.getBackwardsIterator();
+    
+   while (!fi->isFinished()) {
+       std::cout << fi->next() << ", ";
+   }
+   
+   std::cout << "\n";
+   
+   std::cout << "Iterating through the numbers backwards:\n";
+   
+   while (!bi->isFinished()) {
+       std::cout << bi->next() << ", ";
+   }
+   
+   std::cout << "\n";
+
+    delete fi;
+    delete bi;
+    
     return 0;
 }
