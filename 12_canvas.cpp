15f1418c373cbc15f7358f34b5e2ddc9fd155606
zoey-yjj <zoeyyyjj@gmail.com>
2022-11-15 09:46:45 +0800

    cavas done

--- 12_canvas.cpp
+++ 12_canvas.cpp
@@ -18,17 +18,31 @@ class Canvas;
 
 class CanvasMemento {
     friend class Canvas;
-    const std::vector<std::string> shapes;
+    const std::vector<std::string> shapes;  // only canvas class has access to this
 public:
     CanvasMemento(std::vector<std::string> shapes) : shapes(shapes) {};
 };
 
 class Canvas {
     std::vector<std::string> shapes;
+    std::vector<CanvasMemento*> oldStates;
 public:
+    ~Canvas() {
+        for (auto p : oldStates) {
+            delete p;
+        }
+        oldStates.clear();
+    }
     void addShape(const std::string & newShape) {
+        oldStates.push_back(new CanvasMemento(shapes));
         shapes.push_back(newShape);
     };
+    void undo() {
+        CanvasMemento *previousState = oldStates.back();
+        oldStates.pop_back();
+        shapes = previousState->shapes;
+        delete previousState;
+    }
     void clearAll() {
         shapes.clear();
     };
@@ -36,7 +50,40 @@ public:
 };
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    Canvas *canvas = new Canvas;
+    
+    canvas->addShape("rhombus");
+    canvas->addShape("triangle");
+    canvas->addShape("square");
+    canvas->addShape("circle");
+    
+    for (auto shape : canvas->getShapes()) {
+        std::cout << shape << ", ";
+    };
+    
+    std::cout << "\n";
+    
+    canvas->undo();
+    
+    for (auto shape : canvas->getShapes()) {
+        std::cout << shape << ", ";
+    };
+    
+    std::cout << "\n";
+    
+    canvas->addShape("rhombus");
+    canvas->addShape("triangle");
+    canvas->addShape("square");
+    canvas->addShape("circle");
+    canvas->undo();
+    
+    for (auto shape : canvas->getShapes()) {
+        std::cout << shape << ", ";
+    };
+    
+    std::cout << "\n";
+ 
+    delete canvas;
+
     return 0;
 }
