e1c7091537d5182b4b538734e55acddf1466eca7
zoey-yjj <zoeyyyjj@gmail.com>
2022-11-16 08:55:48 +0800

    log project init

---
+++ 13_log.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <stdio.h>
+
+/*
+ * Null Object pattern
+ *
+ * 1. Interact with instances of a class without worrying
+ *    whether or not they exist.
+ * 2. For a given class, define an acceptable null value, such that
+ *    interact with the class without it having to actually exist.
+ * 
+ * Logger Interface
+ * ---------------------
+ * + write(info)
+ */
+
+class Logger {
+public:
+    virtual ~Logger() {};
+    virtual void log(const std::string & message) = 0;
+};
+
+class ConsoleLogger : public Logger {
+public:
+    void log(const std::string & message) override {
+        std::cout << message << "\n";
+    }
+};
+
+class FileLogger :  public Logger {
+    std::string filePath;
+public:
+    FileLogger(const std::string & filePath) : filePath(filePath) {};
+    void log(const std::string & message) override {
+        std::cout << "Printing message to file " << filePath << ": " << message << "\n";
+    }
+};
+
+class ApiLogger : public Logger {
+    std::string url;
+public:
+    ApiLogger(const std::string & url) : url(url) {};
+    void log(const std::string & message) override {
+        std::cout << "Sending message to api " << url << ": " << message << "\n";
+    }
+};
+
+class NullLogger : public Logger {
+public:
+    void log(const std::string & message) override {};
+};
+
+class SomeTask {
+    Logger *logger = nullptr;
+public:
+    SomeTask(Logger *logger) : logger(logger) {};
+    ~SomeTask() { delete logger; };
+    void execute() {
+        // Do some stuff...
+        
+        if (logger) {
+            logger->log("Did some stuff");
+        }
+        
+        // Do some other stuff...
+        
+        if (logger) {
+            logger->log("Did some other stuff");
+        }
+        
+        // One last thing...
+        
+        if (logger) {
+            logger->log("Task complete!");
+        }
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
